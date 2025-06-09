9252a2f15f4264f8343a45588533153c9997f17e
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2025-06-09 12:03:49 +0000

    conversion

--- Basics_js/conversion_datatype.js
+++ Basics_js/conversion_datatype.js
@@ -15,9 +15,19 @@
    true = 1 false =0 
 */
 
- let isLoggedIn =1
+ //let isLoggedIn =1
 
- let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
+ //let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanIsLoggedIn);
 
- 
+ //*************opertions************//
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue);
+
+let str1 = "hello"
+let str2 = "pratyay"
+
+let str3 = str1+str2
+console.log(str3)
