bb55a6baf1f0fe943731b3469092e4b5058d8662
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2025-06-12 06:57:43 +0000

    conversion

--- Basics_js/conversion_datatype.js
+++ Basics_js/conversion_datatype.js
@@ -22,12 +22,29 @@
 
  //*************opertions************//
 
- let value = 3
- let negValue = -value
- console.log(negValue);
+//  let value = 3
+//  let negValue = -value
+//  console.log(negValue);
 
-let str1 = "hello"
-let str2 = "pratyay"
+// let str1 = "hello"
+// let str2 = "pratyay"
 
-let str3 = str1+str2
-console.log(str3)
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
